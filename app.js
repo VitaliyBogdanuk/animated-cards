3413e58865856a72bace1315efc03cfe9b21baf2
Vitalii Bohdaniuk <vitaliibogdaniuk@MacBook-Pro-Vitalii.local>
2021-12-22 21:11:33 +0200

    changing js to plugin design

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
 
-slides.forEach((slide) => {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    });
-});
-function clearActiveClasses(){
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active');
+
     slides.forEach((slide) => {
-        slide.classList.remove('active');
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        });
     });
-};
+    function clearActiveClasses(){
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    };
+}
+
+slidesPlugin(2);
